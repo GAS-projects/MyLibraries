c2a6df0e7183a41033b908e04bc75ddde5d61841
GAS-projects <128761935+GAS-projects@users.noreply.github.com>
2024-02-02 21:54:09 +0900

    Split.h

---
+++ Libraries/c++/Split関数/Split.h
@@ -0,0 +1,11 @@
+#ifndef _SPLIT_H_
+#define _SPLIT_H_
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void Split(string str, vector<string> *strArray, char splitChar);
+
+#endif
