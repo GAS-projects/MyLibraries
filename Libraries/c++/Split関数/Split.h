3fe4b3dadf34374d6ba962d82616f3a28ed66838
TETTSUN <128761935+GAS-projects@users.noreply.github.com>
2024-02-03 11:40:51 +0900

    Update Split.h

--- Libraries/c++/Split関数/Split.h
+++ Libraries/c++/Split関数/Split.h
@@ -6,8 +6,27 @@
 
 using namespace std;
 
-void Split(string str, vector<string> *strArray, char splitChar);
+void Split(string str, vector<string> *strArray, char splitChar){
+  string word;
+
+  str += splitChar; //最後の語句もしっかりと区切るためにつけている。
+
+  for(int i = 0; i <= str.length(); i++){
+
+    if(str[i] == splitChar){
+
+      strArray -> push_back(word);
+      word = "";
+      continue; //区切り文字をリストの要素中の文字列に入れさせないため、一文字飛ばしている。
+
+    }
+    word += str[i];
+
+  }
+
+  return;
+}
 
 #endif
 
-/*関数のプロトタイプ宣言を行う。Split.cppに、定義が乗っている。*/
+/*関数のプロトタイプ宣言と定義を行う。*/
