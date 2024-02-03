f1287145584eb9d85b2af927029b9e36da3323fa
TETTSUN <128761935+GAS-projects@users.noreply.github.com>
2024-02-03 19:59:29 +0900

    Update Split.h

--- Libraries/c++/Split関数/Split.h
+++ Libraries/c++/Split関数/Split.h
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-void Split(string str, vector<string> *strArray, char splitChar){
+void Split(string str, vector<string> &strArray, char splitChar){
   string word;
 
   str += splitChar; //最後の語句もしっかりと区切るためにつけている。
@@ -15,12 +15,12 @@ void Split(string str, vector<string> *strArray, char splitChar){
 
     if(str[i] == splitChar){
 
-      strArray -> push_back(word);
+      if(word != "") strArray.push_back(word);
       word = "";
       continue; //区切り文字をリストの要素中の文字列に入れさせないため、一文字飛ばしている。
 
     }
-    word += str[i];
+    else word += str[i];
 
   }
 
