dca9eddc9d48ed8baea8a9706d19a3af22d2a52a
TETTSUN <128761935+GAS-projects@users.noreply.github.com>
2024-02-11 23:11:14 +0900

    Update Split.cpp

--- Libraries/c++/Split関数/Split.cpp
+++ Libraries/c++/Split関数/Split.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <vector>
+#include "Split.h"
 
+using namespace std;
+
+void Split(const string &str, vector<string> &strArray, char splitChar){
+
+  string word;
+
+  for(int i = 0; i <= str.length(); i++){
+
+    if(str[i] == splitChar || i == str.length()){
+
+      if(word != "") strArray.push_back(word);
+      word = "";
+      continue; //区切り文字をリストの要素中の文字列に入れさせないため、一文字飛ばしている。
+
+    }
+    else word += str[i];
+
+  }
+
+  return;
+}
